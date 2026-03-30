b167e62335494962237b660f28f2f96b900c0b02
MatheusEmgdam <268079624+MatheusEmgdam@users.noreply.github.com>
2026-03-29 22:20:08 -0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+#Import do comando clear para limpeza do terminal
 import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 while (True):
+    #ENTRADA
     aparelho=input("\n Digite o tipo de aparelho\n")
     clear()
     potência=int(input("Digite a potência do aparelho em Watts\n"))
     clear()
     tempo=int(input("Digite o tempo médio do uso diário em horas\n"))
     clear()
+    #PROCESSAMENTO
     consumoMensal=(potência*tempo*30)/1000
+    #SAÍDA
     print(f"Aparelho: {aparelho} \n Consumo estimado: {consumoMensal}kWh/mês" )
